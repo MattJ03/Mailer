14fc4c8295674475fa79c9e64033f9d1cf79b103
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-02-09 19:46:51 +0000

    model for User and Task and taskcontroller

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'completed', 'user_id'];
+
+    public function user() {
+        return $this->belongTo(User::class);
+    }
 }
--- database/migrations/2025_02_09_192138_create_tasks_table.php
+++ database/migrations/2025_02_09_192138_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->enum('completed', ['yes', 'no'])->default('no');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
